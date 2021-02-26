3a0fc8cd71c593779151ba510d817b9aaa45a7d5
Jason Ko <jekb2019@gmail.com>
2021-02-26 16:06:46 +1300

    handle state change on editor

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -3,13 +3,13 @@ import styles from './editor.module.css';
 import CardEditForm from '../card_edit_form/card_edit_form';
 import CardAddForm from '../card_add_form/card_add_form';
 
-const Editor = ({cards, addCard}) => (
+const Editor = ({cards, addCard, updateCard, deleteCard}) => (
         <section className={styles.editor}>
             <h1 className={styles.title}>Cards</h1>
             <ul>
                 {
-                    cards.map(card => (
-                        <CardEditForm key={card.id}  card={card}/>
+                    Object.keys(cards).map(key => (
+                        <CardEditForm key={key} card={cards[key]} updateCard={updateCard} deleteCard={deleteCard}/>
                     ))
                 }
                 <CardAddForm onAdd={addCard}/>
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -7,41 +7,43 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
-  const [cards, setCards] = useState([
+  const [cards, setCards] = useState(
     {
-      id: '1',
-      name: 'Jason',
-      company: 'Google',
-      theme: 'dark',
-      title: 'Software Engineer',
-      email: 'jason@gmail.com',
-      message: 'Go for it',
-      fileName: 'jason',
-      fileURL: null
-    },
-    {
-      id: '2',
-      name: 'Jason2',
-      company: 'Google',
-      theme: 'light',
-      title: 'Software Engineer',
-      email: 'jason@gmail.com',
-      message: 'Go for it',
-      fileName: 'jason',
-      fileURL: null
-    },
-    {
-      id: '3',
-      name: 'Jason3',
-      company: 'Google',
-      theme: 'colorful',
-      title: 'Software Engineer',
-      email: 'jason@gmail.com',
-      message: 'Go for it',
-      fileName: 'jason',
-      fileURL: null
+      '1': {
+        id: '1',
+          name: 'Jason',
+        company: 'Google',
+        theme: 'dark',
+        title: 'Software Engineer',
+        email: 'jason@gmail.com',
+        message: 'Go for it',
+        fileName: 'jason',
+        fileURL: null
+      },
+      '2': {
+        id: '2',
+        name: 'Jason2',
+        company: 'Google',
+        theme: 'light',
+        title: 'Software Engineer',
+        email: 'jason@gmail.com',
+        message: 'Go for it',
+        fileName: 'jason',
+        fileURL: null
+      },
+      '3': {
+        id: '3',
+        name: 'Jason3',
+        company: 'Google',
+        theme: 'colorful',
+        title: 'Software Engineer',
+        email: 'jason@gmail.com',
+        message: 'Go for it',
+        fileName: 'jason',
+        fileURL: null
+      }  
     }
-  ]);
+  );
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
@@ -55,15 +57,27 @@ const Maker = ({ authService }) => {
     });
   });
 
-  const addCard = (card) => {
-    const updated = [...cards, card];
-    setCards(updated);
+  const createOrUpdateCard = (card) => {
+    setCards(cards => {
+      const updated = {... cards};
+      updated[card.id] = card;
+      return updated;
+    });
   }
+
+  const deleteCard = (card) => {
+    setCards(cards => {
+      const updated = {... cards};
+      delete updated[card.id];
+      return updated;
+    });
+  }
+
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} addCard={addCard}/>
+        <Editor cards={cards} addCard={createOrUpdateCard} updateCard={createOrUpdateCard} deleteCard={deleteCard}/>
         <Preview cards={cards}/>
       </div>
       <Footer />
